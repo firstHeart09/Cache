87d5c09cf8d8cb757b5df3e62e594b83bcabc883
“sunshine-study” <1274455999@163.com>
2024-06-05 10:11:25 +0800

    remove getGem5Mess

--- x86/o3cpu/inst/script.sh
+++ x86/o3cpu/inst/script.sh
@@ -1,2 +1,2 @@
-../../gem5.opt --debug-flags=Exec ./trace_inst.py --binary=../test_static > trace.txt
+../../gem5.opt --debug-flags=Exec ./trace_inst.py --binary=./test_static > trace.txt
 python count_inst.py
